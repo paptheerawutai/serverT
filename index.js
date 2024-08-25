3058941db29aa46c73c7213868dbf2059caca580
DevOAmate <paptheerawutai@gmail.com>
2024-08-26 00:18:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ import routes from "./src/routes/MongoDB.js";
 
 const app = express();
 
-app.use(cors({ 
-    origin: "*"
-  }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
