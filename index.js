73d76be049da07d26402ecb8046774e54a2bcf3c
DevOAmate <paptheerawutai@gmail.com>
2024-08-26 00:07:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ import routes from "./src/routes/MongoDB.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ 
+  //ให้ใช้งานในมือถือได้
+    origin: "*",
+  }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
